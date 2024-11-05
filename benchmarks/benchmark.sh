12c3f33ee48f90620628461c0f05f51678cc4e1d
Daniela Thurnher <dthurnher@ethz.ch>
2024-11-05 09:03:02 +0100

    benchmarking for 10mil, 100thous

--- benchmarks/benchmark.sh
+++ benchmarks/benchmark.sh
@@ -6,15 +6,15 @@
 # rnd: random number generators to use
 for rnd in `seq -1 -1`; do
   # m: number of entries (in millions)
-  for m in `seq 100 100`; do
+  for m in `seq 10 10`; do
     # test: test id
-    for test in `seq 1 20`; do
+    for test in `seq 1 4`; do
       # alg: algorithms to test
-      for alg in 0 2 11 12 44 45 51 116 117 118 119 1086 1156 3086 3156; do
+      for alg in 2 12 117 119; do
         now=$(date +"%T");
         echo ${test} ${now} alg ${alg} size ${m} ${rnd};
         sleep 10;
-        ./bulk-insert-and-query.exe ${m}000000 ${alg} ${rnd};
+        ./bulk-insert-and-query.exe ${m}0000 ${alg} ${rnd};
       done;
     done;
   done;
